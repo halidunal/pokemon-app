001cdffe5fba5757a8256212ed53d114073f571a
halidunal <halidunal@gmail.com>
2020-10-19 18:22:46 +0300

    css updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,34 +10,33 @@ function Card({ pokemon }) {
         borderRadius: 20,
         padding: 50,
         margin: 50,
-        textAlign: "center",
-      }}
-    >
-      <div className="card-inline">
+        textAlign: "center",}}>
+    
+      <div className="card-inline">     
         <div className="media-object">
-          <img src={pokemon.sprites.front_default} alt=""  style={{width:150, marginTop:-50, marginBottom:-15}}/>
-        </div>
-        <div className="card-name">NAME: {pokemon.name.toUpperCase()}</div>
-        <div className="card-types">
-          {pokemon.types.map((type) => {
-            return (
-              <div className="card-type">{type.type.name.toUpperCase()}</div>
-            );
-          })}
+          <img src={pokemon.sprites.front_default} alt=""  className="card-img" style={{width:150, marginTop:-30, marginBottom:10, backgroundColor: "white", borderRadius:100, border:"1px solid"}}/>
         </div>
         <div className="card-info">
-          <div className="card-data card-data-weight">
-            <p className="title">WEIGHT: {pokemon.weight}</p>
-          </div>
-          <div className="card-data card-data-height">
-            <p className="title">HEIGHT: {pokemon.height}</p>
-          </div>
-          <div className="card-data card-data-ability">
-            <p className="title">ABILITY: {pokemon.abilities.name}</p>
-          </div>
-          <div>
-            <button className="btn">Catch Pokemon</button>
-          </div>
+            <div className="card-id">#{pokemon.id.toString().padStart(3,"0")}</div>
+
+            <div className="card-name">{pokemon.name.toUpperCase()}</div>
+
+            <div className="card-types">
+                {pokemon.types.map((type) => {
+                    return (
+                    <div className="card-type">{type.type.name.toUpperCase()}</div>);
+                    })
+                }
+            </div>
+            {/* <div className="card-data card-data-weight">
+                <p className="title">WEIGHT: {pokemon.weight}</p>
+            </div>
+            <div className="card-data card-data-height">
+                <p className="title">HEIGHT: {pokemon.height}</p>
+            </div> */}
+            <div>
+                <button className="btn">Catch Pokemon</button>
+            </div>
         </div>
       </div>
     </div>
