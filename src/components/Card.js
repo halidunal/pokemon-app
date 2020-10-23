902486a78d42d545f2ac95b540fa21fb9a03a481
halidunal <halidunal@gmail.com>
2020-10-23 19:42:49 +0300

    card details

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,28 +2,22 @@ import React, { useState } from "react";
 import "./card.css";
 import alertify from "alertifyjs"
 
-function Card({ pokemon }) {
+function Card({pokemon}) {
 
   const [myPokemons, setMyPokemons] = useState([]);
   
   function catchPokemon(pokemon){
     const newPokemon = myPokemons;
     newPokemon.push({pokemon});
-    setMyPokemons({pokemon: newPokemon})
-    alertify.success(pokemon.name + " added my pokemons")
-    //console.log(myPokemons);
+    setMyPokemons({pokemon: newPokemon});
+    alertify.success(pokemon.name + " added my pokemons");
+    console.log(myPokemons);
   }
 
   return (
     <div
       className="card"
-      style={{
-        cursor: "pointer",
-        borderRadius: 20,
-        padding: 50,
-        margin: 50,
-        textAlign: "center",}}>
-    
+      style={{borderRadius: 20,padding: 50,margin: 50,textAlign: "center",}}> 
       <div className="card-inline">     
         <div className="media-object">
           <img src={pokemon.sprites.front_default} alt=""  className="card-img" style={{width:150, marginTop:-30, marginBottom:10, backgroundColor: "white", borderRadius:100, border:"1px solid"}}/>
@@ -54,8 +48,12 @@ function Card({ pokemon }) {
                 <p className="title">HEIGHT: {pokemon.height}</p>
             </div> */}
             <div>
+              <button className="btn-details" style={{borderRadius:10}}>Details</button>
+            </div>
+            <div>
                 <button className="btn" onClick={()=>catchPokemon(pokemon)}>Catch Pokemon</button>
             </div>
+
         </div>
       </div>
     </div>
